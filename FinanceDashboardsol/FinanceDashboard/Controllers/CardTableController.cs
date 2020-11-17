3f17a6db8de6445bdbcb86ad82288050454142ad
Linu27 <riyabharathigathada@gmail.com>
2020-11-17 18:39:08 +0530

    FinanceCardboard

---
+++ FinanceDashboardsol/FinanceDashboard/Controllers/CardTableController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FinanceDashboard.Models;
+using FinanceDashboard.Productspurchased;
+
+namespace FinanceDashboard.Controllers
+{
+    public class CardTableController : ApiController
+    {
+        private FinanceEntities db = new FinanceEntities();
+
+        // GET: api/CardTable
+        public IQueryable<CardTable> GetCardTables()
+        {
+            return db.CardTables;
+        }
+
+        // GET: api/CardTable/Name
+        [ResponseType(typeof(CardTable))]
+        //[Route("CardTable/Name")]
+        public IHttpActionResult GetCardTable(long id)
+        {
+            CardTable cardTable = db.CardTables.Find(id);
+            if (cardTable == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cardTable);
+        }
+
+        [ResponseType(typeof(Productpurchase))]
+        [Route("api/CardTable/GetProducts")]
+        public IEnumerable<Productpurchase> GetProductspurchased()
+        {
+            List<Productpurchase> productslist = new List<Productpurchase>();
+            var pl = (from p in db.Products
+                      join od in db.OrderDetails on
+                      p.ProductID equals od.ProductID
+                      join o in db.Orders on od.OrderID
+                      equals o.OrderID
+                      select new { od.OrderID,p.ProductName, od.TotalAmount, o.BillAmountperMonth, Balance = od.TotalAmount - o.BillAmountperMonth }
+
+                    ).ToList();
+            foreach (var item in pl)
+            {
+                Productpurchase objpl = new Productpurchase();
+                objpl.OrderID = item.OrderID;
+                objpl.ProductName=item.ProductName;
+                objpl.TotalAmount = item.TotalAmount;
+                objpl.BillAmountperMonth = item.BillAmountperMonth;
+                objpl.Balance = item.Balance;
+
+                productslist.Add(objpl);
+
+            }
+            return productslist;
+        }
+        [Route("api/CardTable/GetTransactions")]
+        public IEnumerable<RecentTransaction> GetRecenttransactions()
+        {
+            List<RecentTransaction> transactionlist = new List<RecentTransaction>();
+            var tl = (from p in db.Products
+                      join od in db.OrderDetails on
+                      p.ProductID equals od.ProductID
+                      join o in db.Orders on od.OrderID
+                      equals o.OrderID
+                      select new { od.OrderID, p.ProductName, o.OrderDate,od.TotalAmount}
+
+                    ).ToList();
+            foreach (var item in tl)
+            {
+                RecentTransaction objtl = new RecentTransaction();
+                objtl.OrderID = item.OrderID;
+                objtl.ProductName = item.ProductName;
+                objtl.OrderDate = item.OrderDate;
+                objtl.TotalAmount = item.TotalAmount;
+
+                transactionlist.Add(objtl);
+
+            }
+            return transactionlist;
+        }
+
+
+    }
+}
---
+++ FinanceDashboardsol/FinanceDashboard/Productspurchased/Productpurchase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace FinanceDashboard.Productspurchased
+{
+    public class Productpurchase
+    {
+        [Key]
+        public int OrderID { get; set; }//OrderDetails.cs
+        public int TotalAmount { get; set; }//OrderDetails.cs
+        public string ProductName { get; set; }//Product.cs
+        public double BillAmountperMonth { get; set; }//Order.cs
+        public double Balance { get; set; }
+
+
+
+    }
+}
